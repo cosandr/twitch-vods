984d18cd4f0bb6ed962dbeb00c3e1fad27a13bd8
Andrei Costescu <andrei@costescu.no>
2020-05-15 15:30:16 +0200

    Downgrade to Kraken API
    New API requires OAuth, I don't want to deal with it now

--- modules/recorder.py
+++ modules/recorder.py
@@ -8,9 +8,10 @@ import pickle
 import re
 import signal
 import sys
-from datetime import datetime
+from datetime import datetime, timezone
 
 from aiohttp import ClientSession
+from dateutil.parser import isoparse
 
 import config as cfg
 from utils import setup_logger
@@ -27,7 +28,8 @@ class Recorder:
         setup_logger(self.logger, 'recorder')
         # --- Logger ---
         self.client_id: str = ''
-        self.user: str = ''
+        self.user_login: str = ''
+        self.user_id: int = 0
         self.timeout: int = 120
         self.dst_path: str = '.'
         self.check_en = asyncio.Event()
@@ -55,15 +57,15 @@ class Recorder:
         if not self.notifier:
             return
         try:
-            await self.notifier.send(content, name=f'Twitch Recorder for user {self.user}')
+            await self.notifier.send(content, name=f'Twitch Recorder for user {self.user_login}')
         except:
             self.logger.exception('Cannot send notification')
 
     def get_env(self):
         """Updates configuration from env variables"""
-        self.user = os.getenv('STREAM_USER')
+        self.user_login = os.getenv('STREAM_USER')
         self.client_id = os.getenv('TWITCH_ID')
-        if not self.user:
+        if not self.user_login:
             self.logger.critical('A user to check is required')
             sys.exit(0)
         if not self.client_id:
@@ -74,7 +76,7 @@ class Recorder:
                 self.timeout = int(timeout)
             except ValueError:
                 self.logger.error('Invalid timeout %s', timeout)
-        self.logger.info('Checking %s every %d seconds', self.user, self.timeout)
+        self.logger.info('Checking %s every %d seconds', self.user_login, self.timeout)
         if env_path := os.getenv('PATH_DST'):
             if not os.path.exists(env_path):
                 self.logger.warning('%s does not exist', env_path)
@@ -102,6 +104,7 @@ class Recorder:
         await self.send_notification('Recorder started')
         self.logger.info("aiohttp session initialized.")
         self.aio_sess = ClientSession()
+        await self.get_user_id()
 
     async def close(self):
         await self.send_notification('Recorder is closing')
@@ -114,55 +117,111 @@ class Recorder:
         await asyncio.sleep(timeout)
         self.check_en.set()
 
-    async def aio_request(self, url, headers, params):
+    async def get_user_id(self):
+        """Sets the user's ID"""
+        url = 'https://api.twitch.tv/kraken/users'
+        headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+        params = {'login': self.user_login}
+        data = await self.aio_request(url=url, headers=headers, params=params)
+        # API call didn't work
+        if not data:
+            await self.send_notification('User ID API failed')
+            self.logger.critical('User ID API failure')
+            raise KeyboardInterrupt
+        for u in data.get('users', []):
+            if u['name'].lower() == self.user_login:
+                self.user_id = u.get('_id', 0)
+        # We didn't find the user ID, likely wrong username given
+        if not self.user_id:
+            self.logger.critical('Cannot get user ID, check username')
+            raise KeyboardInterrupt
+        self.logger.debug(f'Got user ID {self.user_id}')
+
+    async def get_stream_data(self) -> dict:
+        """Returns some stream data if current user is streaming"""
+        url = f'https://api.twitch.tv/kraken/streams/{self.user_id}'
+        headers = {'Client-ID': self.client_id, 'Accept': 'application/vnd.twitchtv.v5+json'}
+        data = await self.aio_request(url=url, headers=headers)
+        # API call didn't work
+        if not data:
+            await self.send_notification('Streams API failed')
+            self.logger.critical('Streams API failure')
+            raise KeyboardInterrupt
+        ret = {}
+        if data.get('stream') and data['stream'].get('stream_type') == 'live':
+            if time_str := data['stream'].get('created_at'):
+                try:
+                    ret['time'] = isoparse(time_str)
+                    # Convert UTC to local time
+                    ret['time'] = ret['time'].replace(tzinfo=timezone.utc).astimezone(tz=None)
+                except Exception as e:
+                    self.logger.warning(f'Cannot parse time string "{time_str}": {e}')
+            if 'time' not in ret:
+                ret['time'] = datetime.now()
+            if data.get('channel') and data['channel'].get('status'):
+                ret['title'] = data['channel']['status']
+            else:
+                ret['title'] = 'UNKNOWN'
+        return ret
+
+    async def get_hosting_target(self) -> str:
+        """Returns who current is hosting, empty string if nobody"""
+        url = "https://tmi.twitch.tv/hosts"
+        params = {'include_logins': 1, 'host': self.user_id}
+        data = await self.aio_request(url=url, params=params)
+        if not data:
+            await self.send_notification('Hosting API failed')
+            self.logger.error('Hosting API failure')
+            return ''
+        if not data.get('hosts') or len(data['hosts']) == 0:
+            return ''
+        return data['hosts'][0].get('target_login', '')
+
+    async def aio_request(self, url, headers=None, params=None) -> dict:
+        """Run HTTP GET"""
         data = None
         try:
             async with self.aio_sess.get(url=url, headers=headers, params=params) as resp:
                 if resp.status == 200:
                     data = await resp.json()
                 else:
-                    self.logger.error('Invalid response %d, retrying in %ds', resp.status, self.timeout)
-                    self.loop.create_task(self.timer())
-                    self.check_en.clear()
+                    self.logger.error(f'Invalid response {resp.status}\n\t{data}')
         except Exception as e:
-            self.logger.error('aiohttp error %s, retrying in %ds', str(e), self.timeout)
-            self.loop.create_task(self.timer())
-            self.check_en.clear()
+            self.logger.error(f'aiohttp error {e}')
         return data
 
     async def check_if_live(self):
-        headers = {'Client-ID': self.client_id}
-        url = "https://api.twitch.tv/helix/streams"
-        params = {'user_login': self.user}
+        """
+        1. Get user ID
+        2. Check for host
+        3. Check for streams
+        4. Run streamlink if appropriate
+        """
         await self.check_en.wait()
-        data = await self.aio_request(url, headers, params)
-        if not data or (len(data['data']) == 0) or (data['data'][0]['type'] != 'live'):
-            self.logger.debug('%s is offline, retrying in %ds', self.user, self.timeout)
+        data = await self.get_stream_data()
+        if not data:
+            self.logger.debug('%s is offline, retrying in %ds', self.user_login, self.timeout)
             self.loop.create_task(self.timer())
             self.check_en.clear()
             return
         # Check if hosting
-        url = "https://tmi.twitch.tv/hosts"
-        params = {'include_logins': 1, 'host': data['data'][0]['user_id']}
-        data_host = await self.aio_request(url=url, headers=None, params=params)
-        host_login = data_host['hosts'][0].get('target_login')
-        if host_login:
-            self.logger.info('%s is live but hosting %s, retrying in 10m.', self.user, host_login)
+        if host_login := await self.get_hosting_target():
+            self.logger.info('%s is live but hosting %s, retrying in 10m.', self.user_login, host_login)
             self.loop.create_task(self.timer(600))
             self.check_en.clear()
             return
-        self.title = data['data'][0]['title'].replace("/", "")
-        self.start_time = datetime.now()
+        self.title = data['title'].replace("/", "")
+        self.start_time = data['time']
         self.start_time_str = self.start_time.strftime(cfg.TIME_FMT)
-        self.logger.info('%s is live: %s', self.user, self.title)
+        self.logger.info('%s is live: %s', self.user_login, self.title)
         await self.record()
 
     async def record(self):
         self.check_en.clear()
         no_space_title = re.sub(r'[^a-zA-Z0-9]+', '_', self.title)
-        rec_name = F"{self.start_time_str}_{self.user}_{no_space_title}"
+        rec_name = F"{self.start_time_str}_{self.user_login}_{no_space_title}"
         raw_fp = os.path.join(self.dst_path, f'{rec_name}.flv')
-        stream_url = F"twitch.tv/{self.user}"
+        stream_url = F"twitch.tv/{self.user_login}"
         self.logger.info('Saving raw stream to %s', raw_fp)
         cmd = F"streamlink {stream_url} --default-stream best -o {raw_fp} -l info"
         await self.send_notification(f'Live stream recording started: {self.title}')
@@ -182,7 +241,7 @@ class Recorder:
         win_title = re.sub(r'(\s{2,}|\s+$|[<>:\"/\\|?*\n]+)', '', self.title)
         conv_name = F"{self.start_time_str}_{win_title}"
         # Send job to encoder
-        send_dict = {'src': raw_fp, 'file_name': conv_name, 'user': self.user, 'raw': True}
+        send_dict = {'src': raw_fp, 'file_name': conv_name, 'user': self.user_login, 'raw': True}
         try:
             await self.send_job(send_dict)
         except Exception as e:
