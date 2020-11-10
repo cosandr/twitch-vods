60f4b3d6e0b9eae93bdb8a226ebcef2fcee92a3e
Andrei Costescu <andrei@costescu.no>
2020-11-10 23:38:14 +0100

    Remove global variables in launcher

--- launcher.py
+++ launcher.py
@@ -9,8 +9,6 @@ from typing import Dict
 
 from modules import Encoder, Recorder, Generator, Notifier, Cleaner
 
-LOOP = asyncio.get_event_loop()
-
 
 def merge_env_args(env_map: dict, args: argparse.Namespace) -> dict:
     """Reads environment variables defined in env_map and overrides them with args if needed"""
@@ -32,7 +30,7 @@ def merge_env_args(env_map: dict, args: argparse.Namespace) -> dict:
     return kwargs
 
 
-def run_cleaner(args: argparse.Namespace):
+def run_cleaner(loop: asyncio.AbstractEventLoop, args: argparse.Namespace):
     env_map = {
         "clean_days": None,
         "no_notifications": None,
@@ -41,16 +39,16 @@ def run_cleaner(args: argparse.Namespace):
         "webhook_url": "WEBHOOK_URL",
     }
     kwargs = merge_env_args(env_map, args)
-    inst = Cleaner(LOOP, check_path=args.path, **kwargs)
+    inst = Cleaner(loop, check_path=args.path, **kwargs)
 
     async def _run():
         await inst.init_task
         await inst.worker_task
 
-    LOOP.run_until_complete(_run())
+    loop.run_until_complete(_run())
 
 
-def run_encoder(args: argparse.Namespace):
+def run_encoder(loop: asyncio.AbstractEventLoop, args: argparse.Namespace):
     env_map = {
         "clean_days": None,
         "copy_pattern": None,
@@ -69,11 +67,11 @@ def run_encoder(args: argparse.Namespace):
         "webhook_url": "WEBHOOK_URL",
     }
     kwargs = merge_env_args(env_map, args)
-    inst = Encoder(LOOP, **kwargs)
+    inst = Encoder(loop, **kwargs)
     inst.run_app()
 
 
-def run_generator(args: argparse.Namespace):
+def run_generator(loop: asyncio.AbstractEventLoop, args: argparse.Namespace):
     env_map = {
         "out_path": "GEN_DST",
         "pg_uri": "GEN_PG_URI",
@@ -99,7 +97,7 @@ def run_generator(args: argparse.Namespace):
         raise RuntimeError('Source path(s) required')
     elif not kwargs.get('out_path'):
         raise RuntimeError('Output path required')
-    inst = Generator(LOOP, **kwargs)
+    inst = Generator(loop, **kwargs)
 
     async def _run():
         await inst.init_task
@@ -112,26 +110,26 @@ def run_generator(args: argparse.Namespace):
             traceback.print_exception(type(error), error, error.__traceback__)
         await inst.close()
 
-    LOOP.run_until_complete(_run())
+    loop.run_until_complete(_run())
 
 
-def run_notifier(args: argparse.Namespace):
+def run_notifier(loop: asyncio.AbstractEventLoop, args: argparse.Namespace):
     env_map = {
         "mention_id": "NOT_MENTION_ID",
         "webhook_url": "WEBHOOK_URL",
     }
     kwargs = merge_env_args(env_map, args)
     content = ' '.join(args.content)
-    inst = Notifier(LOOP, **kwargs)
+    inst = Notifier(loop, **kwargs)
 
     async def _run():
         await inst.init_task
         await inst.send(content=content)
 
-    LOOP.run_until_complete(_run())
+    loop.run_until_complete(_run())
 
 
-def run_recorder(args: argparse.Namespace):
+def run_recorder(loop: asyncio.AbstractEventLoop, args: argparse.Namespace):
     env_map = {
         "dry_run": None,
         "enc_path": "ENC_PATH",
@@ -144,7 +142,7 @@ def run_recorder(args: argparse.Namespace):
         "webhook_url": "WEBHOOK_URL",
     }
     kwargs = merge_env_args(env_map, args)
-    inst = Recorder(loop=LOOP, **kwargs)
+    inst = Recorder(loop=loop, **kwargs)
 
     async def _run():
         await inst.init_task
@@ -159,63 +157,71 @@ def run_recorder(args: argparse.Namespace):
                 pass
         await inst.close()
 
-    LOOP.run_until_complete(_run())
+    loop.run_until_complete(_run())
 
 
-parser = argparse.ArgumentParser(description='Launcher for twitch recorder stuff')
-# Global options
-grp_global = parser.add_argument_group(title='Global options')
-grp_global.add_argument('-n', '--dry_run', action='store_true', default=False, help='Do not change any files')
-grp_global.add_argument('--no_notifications', action='store_true', default=False, help='Disable Discord notifications')
-grp_global.add_argument('--time_format', type=str, default='%y%m%d-%H%M', help='Time format string for videos, must not contain _')
-grp_global.add_argument('--webhook_url', type=str, help='Discord webhook URL')
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Launcher for twitch recorder stuff')
+    # Global options
+    grp_global = parser.add_argument_group(title='Global options')
+    grp_global.add_argument('-n', '--dry_run', action='store_true', default=False, help='Do not change any files')
+    grp_global.add_argument('--no_notifications', action='store_true', default=False, help='Disable Discord notifications')
+    grp_global.add_argument('--time_format', type=str, default='%y%m%d-%H%M', help='Time format string for videos, must not contain _')
+    grp_global.add_argument('--webhook_url', type=str, help='Discord webhook URL')
 
-# Cleaner options
-grp_cleaner = parser.add_argument_group(title='Cleaner options', description='Applies to standalone cleaner and encoder')
-grp_cleaner.add_argument('-d', '--clean_days', type=int, required=False, help='How many days until a file gets deleted')
-grp_cleaner.add_argument('-w', '--warn_at', type=int, action='append', help='Hours before deletion time to send notifications')
+    # Cleaner options
+    grp_cleaner = parser.add_argument_group(title='Cleaner options', description='Applies to standalone cleaner and encoder')
+    grp_cleaner.add_argument('-d', '--clean_days', type=int, required=False, help='How many days until a file gets deleted')
+    grp_cleaner.add_argument('-w', '--warn_at', type=int, action='append', help='Hours before deletion time to send notifications')
 
-subparsers = parser.add_subparsers(title='Commands', required=True)
+    subparsers = parser.add_subparsers(title='Commands', required=True)
 
-parser_cln = subparsers.add_parser('cleaner', help='Start cleaner, usually started by encoder')
-parser_cln.add_argument('-p', '--path', type=str, required=True, help='Path to check')
-parser_cln.set_defaults(func=run_cleaner)
+    parser_cln = subparsers.add_parser('cleaner', help='Start cleaner, usually started by encoder')
+    parser_cln.add_argument('-p', '--path', type=str, required=True, help='Path to check')
+    parser_cln.set_defaults(func=run_cleaner)
 
-parser_enc = subparsers.add_parser('encoder', help='Start encoder REST API server')
-parser_enc.add_argument('-i', '--src_path', type=str, help='Source file location')
-parser_enc.add_argument('-o', '--out_path', type=str, help='Processed file location')
-parser_enc.add_argument('--copy_pattern', type=str, required=False, help='Regex pattern for copying files')
-parser_enc.add_argument('--copy_pattern_opt', type=str, action='append', help='Python Regex compile options, case sensitive')
-parser_enc.add_argument('--enable_cleaner', action='store_true', help='Delete old files not included in processing pattern')
-parser_enc.add_argument('--hevc_pattern', type=str, required=False, help='Regex pattern for HEVC transcoding, takes precedence')
-parser_enc.add_argument('--hevc_pattern_opt', type=str, action='append', help='Python Regex compile options, case sensitive')
-parser_enc.add_argument('--listen_address', type=str, required=False, help='Absolute path (socket) or IP:PORT')
-parser_enc.add_argument('--print_every', action='store_true', help='Print progress after encoding X seconds')
-parser_enc.set_defaults(func=run_encoder)
+    parser_enc = subparsers.add_parser('encoder', help='Start encoder REST API server')
+    parser_enc.add_argument('-i', '--src_path', type=str, help='Source file location')
+    parser_enc.add_argument('-o', '--out_path', type=str, help='Processed file location')
+    parser_enc.add_argument('--copy_pattern', type=str, required=False, help='Regex pattern for copying files')
+    parser_enc.add_argument('--copy_pattern_opt', type=str, action='append', help='Python Regex compile options, case sensitive')
+    parser_enc.add_argument('--enable_cleaner', action='store_true', help='Delete old files not included in processing pattern')
+    parser_enc.add_argument('--hevc_pattern', type=str, required=False, help='Regex pattern for HEVC transcoding, takes precedence')
+    parser_enc.add_argument('--hevc_pattern_opt', type=str, action='append', help='Python Regex compile options, case sensitive')
+    parser_enc.add_argument('--listen_address', type=str, required=False, help='Absolute path (socket) or IP:PORT')
+    parser_enc.add_argument('--print_every', action='store_true', help='Print progress after encoding X seconds')
+    parser_enc.set_defaults(func=run_encoder)
 
-parser_gen = subparsers.add_parser('generator', help='Start UUID generator')
-parser_gen.add_argument('-i', '--src', type=str, action='append', help='Source path, formatted as [<type>]<path>')
-parser_gen.add_argument('-o', '--out_path', type=str, required=False, help='Path where symlinks are placed')
-parser_gen.add_argument('-p', '--pg_uri', type=str, required=False, help='PostgreSQL URI')
-parser_gen.set_defaults(func=run_generator)
+    parser_gen = subparsers.add_parser('generator', help='Start UUID generator')
+    parser_gen.add_argument('-i', '--src', type=str, action='append', help='Source path, formatted as [<type>]<path>')
+    parser_gen.add_argument('-o', '--out_path', type=str, required=False, help='Path where symlinks are placed')
+    parser_gen.add_argument('-p', '--pg_uri', type=str, required=False, help='PostgreSQL URI')
+    parser_gen.set_defaults(func=run_generator)
 
-parser_not = subparsers.add_parser('notifier', help='Start Discord notifier, primarily for testing')
-parser_not.add_argument('-c', '--content', nargs='+', required=True, help='Content to send')
-parser_not.add_argument('-m', '--mention-id', type=str, help='User ID to mention')
-parser_not.set_defaults(func=run_notifier)
+    parser_not = subparsers.add_parser('notifier', help='Start Discord notifier, primarily for testing')
+    parser_not.add_argument('-c', '--content', nargs='+', required=True, help='Content to send')
+    parser_not.add_argument('-m', '--mention-id', type=str, help='User ID to mention')
+    parser_not.set_defaults(func=run_notifier)
 
-parser_rec = subparsers.add_parser('recorder', help='Start Twitch recorder')
-parser_rec.add_argument('-u', '--user', type=str, required=False, help='User to record')
-parser_rec.add_argument('-tid', '--twitch_id', type=str, required=False, help='Twitch API Client-ID')
-parser_rec.add_argument('-t', '--timeout', type=int, required=False, help='Time between live checks')
-parser_rec.add_argument('-o', '--out_path', type=str, required=False, help='Output directory of raw recordings')
-parser_rec.add_argument('-e', '--enc_path', type=str, required=False, help='Path/URL to encoder API')
-parser_rec.set_defaults(func=run_recorder)
+    parser_rec = subparsers.add_parser('recorder', help='Start Twitch recorder')
+    parser_rec.add_argument('-u', '--user', type=str, required=False, help='User to record')
+    parser_rec.add_argument('-tid', '--twitch_id', type=str, required=False, help='Twitch API Client-ID')
+    parser_rec.add_argument('-t', '--timeout', type=int, required=False, help='Time between live checks')
+    parser_rec.add_argument('-o', '--out_path', type=str, required=False, help='Output directory of raw recordings')
+    parser_rec.add_argument('-e', '--enc_path', type=str, required=False, help='Path/URL to encoder API')
+    parser_rec.set_defaults(func=run_recorder)
+
+    return parser.parse_args()
 
 
-if __name__ == '__main__':
+def main():
     for folder in ('data', 'log'):
         if not os.path.exists(folder):
             os.mkdir(folder)
-    _args = parser.parse_args()
-    _args.func(_args)
+    args = parse_args()
+    loop = asyncio.get_event_loop()
+    args.func(loop, args)
+
+
+if __name__ == '__main__':
+    main()
